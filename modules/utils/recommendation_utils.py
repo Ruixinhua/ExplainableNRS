5ae3dcf12ef31d42624ef3bfda4205d6a80677a8
Liu Dairui <dairui.liu@ucdconnect.ie>
2023-03-15 21:00:58 +0000

    update description

--- modules/utils/recommendation_utils.py
+++ modules/utils/recommendation_utils.py
@@ -1,5 +1,5 @@
 from tqdm import tqdm
-from modules.utils import convert_dict_to_numpy, gather_dict, load_batch_data
+from modules.utils import convert_dict_to_numpy, gather_dict, load_batch_data, gpu_stat
 
 
 def get_news_embeds(model, news_loader, **kwargs):
@@ -15,11 +15,13 @@ def get_news_embeds(model, news_loader, **kwargs):
     device = kwargs.get("device")
     if accelerator:
         news_loader = accelerator.prepare_data_loader(news_loader)
-    for batch_dict in tqdm(news_loader, total=len(news_loader), desc="Get news embeddings"):
+    bar = tqdm(news_loader, total=len(news_loader))
+    for batch_dict in bar:
         # load data to device
         batch_dict = load_batch_data(batch_dict, device)
         # run news encoder
         news_vec = model.news_encoder(batch_dict)["news_embed"]
+        bar.set_description(f"Get news embeddings: {gpu_stat()}")
         # update news vectors
         news_embeds.update(dict(zip(batch_dict["index"].cpu().tolist(), news_vec.cpu().numpy())))
     return convert_dict_to_numpy(gather_dict(news_embeds))
